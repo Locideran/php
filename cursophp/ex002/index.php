54f6700abc4c9f1335709c1c984dcadf232f4ceb
locideran <gabriel-masc@hotmail.com>
2024-09-30 18:25:22 -0300

    raiva

--- cursophp/ex002/index.php
+++ cursophp/ex002/index.php
@@ -14,6 +14,8 @@
         date_default_timezone_set("America/Sao_Paulo");
         echo "Hoje é dia " . date("d/M/Y");
         echo " e a hora atual é " . date("G:i:s T");
-    ?>        
+    ?>   
+    <p>ola</p>
+             
 </body>
 </html>
